419e3e6966ddddc1411cd871e3ca8d3d9b3ab048
erika0915 <soohee2001@gmail.com>
2025-02-04 16:57:51 +0900

    Initial commit

---
+++ src/main/java/gdg_5team/demo/Gdg5teamApplication.java
@@ -0,0 +1,13 @@
+package gdg_5team.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdg5teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdg5teamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdg_5team/demo/Gdg5teamApplicationTests.java
@@ -0,0 +1,13 @@
+package gdg_5team.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdg5teamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
